ae01c56fa0ff65e71c320ab2847864c1c7e099fd
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-01-28 00:08:33 +0530

    started working on the project

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLoactionState createState() => _ChooseLoactionState();
+}
+ 
+class _ChooseLoactionState extends State<ChooseLocation>{
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:Text('Choose Location Screen')
+    );
+  }
+}
