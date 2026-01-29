24a7344bf7a54df6b59df8b9cb1283fdfc8bb0c3
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-01-29 23:08:32 +0530

    added a dot env file to secure the private api

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:world_time_app/pages/home.dart';
 import 'package:world_time_app/pages/loading.dart';
 import 'package:world_time_app/pages/choose_location.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 
-void main() {
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+await dotenv.load(fileName: ".env");
+
+
   runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
    initialRoute:'/',
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,55 +1,62 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:intl/intl.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class WorldTime {
-  String location; //location name 
-  String time = ''; //the time 
-  String flag; //flags adderess
-  String url; //location addres for api 
-  bool isDaytime = true; //true or false if daytime 
+  String location;
+  late String time;
+  String flag;
+  String url;
+  bool isDaytime = true;
 
   WorldTime({
     required this.location,
     required this.flag,
-    required this.url
-      });
+    required this.url,
+  });
 
   Future<void> getTime() async {
     try {
-      var uri = Uri.parse('https://world-time-api3.p.rapidapi.com/timezone/$url');
+      final apiKey = dotenv.env['RAPIDAPI_KEY'];
+      final apiHost = dotenv.env['RAPIDAPI_HOST'];
+      final baseUrl = dotenv.env['API_BASE_URL'];
 
-      var response = await http.get(uri, headers: {
-        'x-rapidapi-key': 'b1503640a7msh3136e0dee6d04efp1644c0jsnbae612c258aa',
-        'x-rapidapi-host': 'world-time-api3.p.rapidapi.com',
-      });
+      final uri = Uri.parse('$baseUrl$url');
 
-      Map data = jsonDecode(response.body);
+      final response = await http.get(
+        uri,
+        headers: {
+          'x-rapidapi-key': apiKey!,
+          'x-rapidapi-host': apiHost!,
+        },
+      );
 
-      String datetime = data['datetime'];
-      String offset = data['utc_offset'];
+      if (response.statusCode != 200) {
+        throw Exception('API Error: ${response.statusCode}');
+      }
+
+      final data = jsonDecode(response.body);
+
+      final String datetime = data['datetime'];
+      final String offset = data['utc_offset'];
 
       DateTime now = DateTime.parse(datetime);
-      int offsetHours = int.parse(offset.substring(1, 3));
-      int offsetMinutes = int.parse(offset.substring(4, 6));
+      final int offsetHours = int.parse(offset.substring(1, 3));
+      final int offsetMinutes = int.parse(offset.substring(4, 6));
 
-      if (offset.contains('+')) {
-      now = now.add(Duration(hours: offsetHours, minutes: offsetMinutes));
-    } else {
-      now =now.subtract(Duration(hours: offsetHours, minutes: offsetMinutes));
-    }
-      //set the time property
-      isDaytime = now.hour > 6 && now.hour < 20 ? true : false; // 6 am - 8 pm
+      if (offset.startsWith('+')) {
+        now = now.add(Duration(hours: offsetHours, minutes: offsetMinutes));
+      } else {
+        now = now.subtract(Duration(hours: offsetHours, minutes: offsetMinutes));
+      }
+
+      isDaytime = now.hour >= 6 && now.hour < 20;
       time = DateFormat.jm().format(now);
 
-
-
     } catch (e) {
-      print('Caught error: $e');
-      time = 'Could not get time data';
+      time = 'Error fetching time';
+      print('WorldTime error: $e');
     }
   }
 }
-
-//WorldTime instance = WorldTime(location: 'Colombo', flag: 'g', url: 'Asia/Colombo');
-//instance.getTime();
